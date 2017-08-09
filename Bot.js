41fd2f5394e4fd9739547471849df270df701792
bossthanawat <boss41680@gmail.com>
2017-08-09 15:27:45 +0700

    no message

---
+++ Bot.js
@@ -0,0 +1,54 @@
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+
+app.use(bodyParser.json())
+
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(typeof sender, typeof text)
+  // console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(sender, text)
+  }
+  res.sendStatus(200)
+})
+
+function sendText (sender, text) {
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ 💞'
+      }
+    ]
+  }
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'GdRYM/+wDNxJM4Vv3+QLLuJ0uF/kvgn68U+EcBDvFphk5rbnnLbPg6zZgvbgJT8Pn7wM2ocjbSskjZLMJ7VDNG3nl/4x7sY3rheVN6fOl7+nYRSKlKOUAHi7tiVwMvfGiWFcVpY+Tycig5mxTUQIawdB04t89/1O/w1cDnyilFU='
+    },
+    url: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  })
+}
+
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
